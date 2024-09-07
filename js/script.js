617c821347e6a4d48cdb9af70c40c8025755ce8c
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-09-07 23:13:08 +0330

    Added typed.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Computer Engineering Student", "Backend Developer", "AI Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
